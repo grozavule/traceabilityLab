ab00e075a7ad38acb4cb0771d848586163c34bca
Eric Drake <puturosule@gmail.com>
2022-12-15 14:11:27 -0700

    added an endpoint that calls a non-existent function

--- server/index.js
+++ server/index.js
@@ -25,4 +25,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+app.get('/fake', (req, res) => {
+    try {
+        fakeFunction();
+    } catch(error) {
+        rollbar.error(`fakeFunction doesn't exist`);
+    }
+});
+
 app.listen(SERVER_PORT, () => console.log(`Server running on Port ${SERVER_PORT}`));
